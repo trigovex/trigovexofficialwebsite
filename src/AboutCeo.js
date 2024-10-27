ff85e422d4fadaf6fe42288c096f8a7591d05fa6
saketh-uwc <160566640+saketh-uwc@users.noreply.github.com>
2024-10-27 14:31:25 +0530

    first commit

--- src/AboutCeo.js
+++ src/AboutCeo.js
@@ -5,28 +5,21 @@ import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome
 const AboutCeo = () => {
   return (
     <div className="about-ceo container my-5">
+    <h1 style={{ textAlign: 'center', fontSize:'2.8rem', color:'black', marginBottom:'30px'}}>About Us</h1> {/* Center the H1 heading */}
       <div className="row align-items-center">
-        <div className="col-md-8">
-        <h1 className="heading-primary">Meet the CEO</h1>
-          <div className="ceo-info p-4 mt-3">
-            <h2 className="heading-secondary">Saketh Dwaraka Gandham</h2>
-            <p className="text-muted">
-              Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
-            </p>
-            <button className="contact-button mt-3">Contact Saketh</button>
-          </div>
-        </div>
-        <div className="col-md-4 text-center">
-          <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
-        </div>
-      </div>
-      <div className="row align-items-center">
-        <div className="col-md-4 text-center">
+      <img 
+                  src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
+                  alt="Horizontal Separator"
+                  style={{ width: '100%', height: '100px', transform: 'scale(1)' }}
+                />
+
+      <div className="col-md-4 text-center">
           <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
         </div>
         <div className="col-md-8">
-        <h1 className="heading-primary">Meet the COO</h1>
+        
           <div className="ceo-info p-4 mt-3">
+          <h1 className="heading-primary">Meet the CEO</h1>
             <h2 className="heading-secondary">Saketh Dwaraka Gandham</h2>
             <p className="text-muted">
               Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
@@ -36,10 +29,19 @@ const AboutCeo = () => {
         </div>
         
       </div>
-      <div className="row align-items-center">
-        <h1 className="heading-primary">Meet the CMO</h1>
+      <img 
+                  src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
+                  alt="Horizontal Separator" 
+                  style={{ width: '100%', height: '100px', transform: 'scale(-1)' }}
+                />
+      <div className="row align-items-center" style={{paddingTop:'30px'}}>
+        
+        <div className="col-md-4 text-center">
+          <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
+        </div>
         <div className="col-md-8">
           <div className="ceo-info p-4 mt-3">
+          <h1 className="heading-primary">Meet the CMO</h1>
             <h2 className="heading-secondary">Saketh Dwaraka Gandham</h2>
             <p className="text-muted">
               Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
@@ -47,9 +49,12 @@ const AboutCeo = () => {
             <button className="contact-button mt-3">Contact Saketh</button>
           </div>
         </div>
-        <div className="col-md-4 text-center">
-          <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
-        </div>
+        <img 
+                  src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
+                  alt="Horizontal Separator"  
+                  style={{ width: '100%', height: '100px', transform: 'scale(1)' }}
+                />
+        
       </div>
     </div>
   );
