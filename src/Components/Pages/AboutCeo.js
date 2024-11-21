6790bf08881d0783a3d0677b714198c646db46dd
Ganesh-346 <saiganeshbhimavarapu88@gmail.com>
2024-11-21 14:36:02 +0530

    changes in about ceo

--- src/Components/Pages/AboutCeo.js
+++ src/Components/Pages/AboutCeo.js
@@ -1,68 +1,80 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import { pageTransitions } from '../common/transitions';
-import './AboutCeo.css'; // Ensure this file contains the necessary styles
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import './AboutCeo.css';
 
 const AboutCeo = () => {
   return (
-    <motion.div
-      className="page-container" // This class will have the padding applied
-      variants={pageTransitions}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={{ duration: 0.3 }}
-    >
-      <div className="about-ceo container mt-5 ">
-        <h1 style={{ textAlign: 'center', fontSize: '2.8rem', color: 'black', marginBottom: '10px', marginTop:'100px' }}>About Us</h1>
-        <div className="row align-items-center">
+    <div className="about-ceo-container">
+      <h1 className="main-title">About Us</h1>
+      
+      {/* CEO Section */}
+      <div className="profile-card">
+        <div className="profile-image">
           <img 
-            src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
-            alt="Horizontal Separator"
-            style={{ width: '100%', height: '100px', transform: 'scale(1)' }}
+            src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" 
+            alt="CEO" 
           />
-          <div className="col-md-4 text-center">
-            <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
-          </div>
-          <div className="col-md-8">
-            <div className="ceo-info p-4 mt-5">
-              <h1 className="heading-primary mt-3">Meet the CEO</h1>
-              <h2 className="heading-secondary">Saketh Dwaraka Gandham</h2>
-              <p className="text-muted">
-                Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
-              </p>
-              <button className="contact-button mt-3">Contact Saketh</button>
-            </div>
-          </div>
         </div>
-        <img 
-          src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
-          alt="Horizontal Separator" 
-          style={{ width: '100%', height: '100px', transform: 'scale(-1)' }}
-        />
-        <div className="row align-items-center" style={{ paddingTop: '30px' }}>
-          <div className="col-md-4 text-center">
-            <img src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" alt="CEO" className="ceo-image rounded-circle" />
-          </div>
-          <div className="col-md-8">
-            <div className="ceo-info p-4 mt-3">
-              <h1 className="heading-primary">Meet the CMO</h1>
-              <h2 className="heading-secondary">Saketh Dwaraka Gandham</h2>
-              <p className="text-muted">
-                Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
-              </p>
-              <button className="contact-button mt-3">Contact Saketh</button>
-            </div>
-          </div>
+        <div className="profile-content">
+          <h2 className="profile-title">Meet the CEO & Founder</h2>
+          <h3 className="profile-name">Saketh Dwaraka Gandham</h3>
+          <p className="profile-description">
+            Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
+          </p>
+          <button className="contact-btn">Contact Saketh</button>
+        </div>
+      </div>
+
+      {/* CMO Section */}
+      {/* <div className="profile-card">
+        <div className="profile-content">
+          <h2 className="profile-title">Meet</h2>
+          <h3 className="profile-name">Dora Babu Bommireddy</h3>
+          <p className="profile-description">
+            Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
+          </p>
+          <button className="contact-btn">Contact Saketh</button>
+        </div>
+        <div className="profile-image">
           <img 
-            src="https://adya.ai/assets/FullHorizontalLine-f691a6cb.svg" 
-            alt="Horizontal Separator"  
-            style={{ width: '100%', height: '100px', transform: 'scale(1)' }}
+            src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" 
+            alt="CMO" 
           />
         </div>
       </div>
-    </motion.div>
+      <div className="profile-card">
+        <div className="profile-image">
+          <img 
+            src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" 
+            alt="CEO" 
+          />
+        </div>
+        <div className="profile-content">
+          <h2 className="profile-title">Meet the CEO & Founder</h2>
+          <h3 className="profile-name">Saketh Dwaraka Gandham</h3>
+          <p className="profile-description">
+            Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
+          </p>
+          <button className="contact-btn">Contact Saketh</button>
+        </div>
+      </div>
+      <div className="profile-card">
+        <div className="profile-content">
+          <h2 className="profile-title">Meet</h2>
+          <h3 className="profile-name">Dora Babu Bommireddy</h3>
+          <p className="profile-description">
+            Welcome to our software development company! Leading our organization with vision and determination is Saketh Dwaraka Gandham, a dynamic leader with a fervent passion for technology and innovation. Armed with a Bachelor of Technology in Electronics and Communication Engineering (ECE), Saketh brings a wealth of expertise to drive our team towards success in the ever-evolving landscape of software development.
+          </p>
+          <button className="contact-btn">Contact Saketh</button>
+        </div>
+        <div className="profile-image">
+          <img 
+            src="https://media.licdn.com/dms/image/v2/D4D03AQEQujwvNkQ2VQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1719767758276?e=1735776000&v=beta&t=SqpuRIbnw_MVia_RlF4sTjnYJH7xgE_yfWS7vMmAPNI" 
+            alt="CMO" 
+          />
+        </div>
+      </div> */}
+      
+    </div>
   );
 };
 
